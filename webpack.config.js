f6c2f0055c949543e7dea3c7bf8eaaa71b5e08ec
vignesh anand <vigneshanand17@gmail.com>
2019-06-04 20:06:56 +0530

    Removed svg-loader

--- webpack.config.js
+++ webpack.config.js
@@ -44,20 +44,6 @@ module.exports = () => ({
           context: '',
           useRelativePath: true
         }
-      },
-      {
-        test: /\.svg$/,
-        use: [
-          {
-            loader: 'babel-loader'
-          },
-          {
-            loader: 'react-svg-loader',
-            options: {
-              jsx: true // true outputs JSX tags
-            }
-          }
-        ]
       }
     ]
   },
