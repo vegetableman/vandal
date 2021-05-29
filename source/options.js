80b7f885a80b9b595f50ecd6e5b83077f5cf6ab8
vignesh anand <vigneshanand17@gmail.com>
2021-05-29 15:24:41 +0530

    Fixed options css

--- source/options.js
+++ source/options.js
@@ -1,7 +1,18 @@
 const historyKey = "__VANDAL__HIST__STORAGE__ISENABLED";
+let timeout;
 
 function clearHistory() {
-  browser.storage.local.clear();
+  browser.storage.local.clear().then(() => {
+    document.getElementById("clear-message").style.visibility = "visible";
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+    timeout = setTimeout(() => {
+      document.getElementById("clear-message").style.visibility = "hidden";
+    }, 2000);
+  }, () => {
+    document.getElementById("clear-message").innerHTML = "Some Error Occured!.";
+  });
 }
 
 function toggleHistory() {
